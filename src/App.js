acdce00e44b48481b7e76e690c6754a44499e717
Kaustuv <karkikaustuv@gmail.com>
2020-08-14 17:42:32 +0545

    sidebar -completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="app">
-
+      <h1>This is the twitter clone</h1>
+      <Sidebar/>
     </div>
   );
 }
